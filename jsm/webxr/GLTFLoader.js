031759c81f4eec68650ab9c1d2b1d5873cdb2fb0
bouguibougua <robinb20137@gmail.com>
2026-09-10 15:55:07 +0200

    commit 1

--- jsm/webxr/GLTFLoader.js
+++ jsm/webxr/GLTFLoader.js
@@ -65,8 +65,8 @@ import {
 	SRGBColorSpace,
 	InstancedBufferAttribute
 } from 'three';
-import { toTrianglesDrawMode } from '../utils/BufferGeometryUtils.js';
-import { clone } from '../utils/SkeletonUtils.js';
+import { toTrianglesDrawMode } from '../BufferGeometryUtils.js';
+import { clone } from '../SkeletonUtils.js';
 
 /**
  * A loader for the glTF 2.0 format.
